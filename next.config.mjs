23b10b0f79373312e8e1938e535e81da73326e23
RomanoBoldrin <vitorromanoboldrin@hotmail.com>
2026-04-18 12:05:44 -0300

    Add redirect from `/` to `home` page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+
+  // Redirect users from the root URL to the /home page
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
