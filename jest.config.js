968a577f3aa167766766587c5885978eeb66f695
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-05-08 14:48:33 -0300

    chore(jest): add `moduleNameMapper` to resolve import paths

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,11 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>"],
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  moduleNameMapper: {
+    // Maps imports starting with "@/..." to the project's src directory
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
