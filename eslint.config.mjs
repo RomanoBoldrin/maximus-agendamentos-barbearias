7e331208f72a3fa60761b9b22e4298e56d76b292
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-05-27 12:58:55 -0300

    chore(eslint): add proper ignores and file-specific flat config rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,94 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import jest from "eslint-plugin-jest";
 import prettier from "eslint-config-prettier/flat";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-
-  prettier,
-
+export default defineConfig([
   globalIgnores([
+    "node_modules/**",
+
     ".next/**",
     "out/**",
     "build/**",
     "dist/**",
     "coverage/**",
+
     ".swc/**",
-
-    "node_modules/**",
-
     "src/generated/**",
     "prisma/migrations/**",
 
+    "package-lock.json",
     "next-env.d.ts",
   ]),
-]);
 
-export default eslintConfig;
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  ...nextVitals,
+
+  {
+    files: [
+      "src/tests/**/*.test.js",
+      "jest.config.js",
+      "jest.setup.js",
+      "jest.globalSetup.js",
+    ],
+    ...jest.configs["flat/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"],
+  },
+
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+
+  prettier,
+]);
